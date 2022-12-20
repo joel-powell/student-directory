3fbbe1d66800665fa8542088414cd5ac4bcd6c4a
joel-powell <joelhpowell@icloud.com>
2022-12-20 11:05:20 +0000

    Add ability to specify filename upon load and save

--- directory.rb
+++ directory.rb
@@ -1,21 +1,21 @@
-require 'csv'
+require "csv"
 
 @students = [] # an empty array accessible to all methods
 
 def print_menu
   puts [
-         "1. Input the students",
-         "2. Show the students",
-         "3. Save the list to students.csv",
-         "4. Load the list from students.csv",
-         "9. Exit" # 9 because we'll be adding more items
-       ]
+    "1. Input students",
+    "2. Show students",
+    "3. Save list to file",
+    "4. Load list from file",
+    "9. Exit" # 9 because we'll be adding more items
+  ]
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(STDIN.gets.chomp)
+    process($stdin.gets.chomp)
   end
 end
 
@@ -40,18 +40,20 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = STDIN.gets.chomp
+  name = $stdin.gets.chomp
   cohort = :november
+  student = [name, cohort]
   # while the name is not empty, repeat this code
   until name.empty?
-    add_students(name, cohort)
+    add_students(student)
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    name = $stdin.gets.chomp
   end
 end
 
-def add_students(name, cohort)
+def add_students(student)
+  name, cohort = student
   # add the student hash to the array
   @students << { name: name, cohort: cohort }
 end
@@ -75,33 +77,54 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+  puts "-------------"
+end
+
+def enter_filename
+  puts "Please enter a CSV filename"
+  $stdin.gets.chomp
 end
 
 def save_students
-  CSV.open("students.csv", "wb") do |csv|
+  filename = enter_filename
+  save_to_file(filename)
+end
+
+def load_students
+  filename = enter_filename
+  try_load_students(filename)
+end
+
+def start_load_students
+  filename = ARGV.shift || "students.csv" # first cli argument or default
+  try_load_students(filename)
+end
+
+def try_load_students(filename)
+  if File.exist?(filename)
+    load_from_file(filename)
+    student_count = CSV.read(filename).length
+    puts "Loaded #{student_count} from #{filename}"
+    puts "Now we have #{@students.count} students"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+def save_to_file(filename)
+  CSV.open(filename, "wb") do |csv|
     @students.each do |student|
       csv << student.values
     end
   end
 end
 
-def load_students(filename = "students.csv")
-  CSV.foreach(filename) do |name, cohort|
-    add_students(name, cohort)
+def load_from_file(filename)
+  CSV.foreach(filename) do |row|
+    add_students(row)
   end
 end
 
-def try_load_students
-  filename = ARGV.shift || "students.csv" # first argument from the command line or default
-
-  if File.exist?(filename) # if it exists
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
-  end
-end
-
-try_load_students
+start_load_students
 interactive_menu
